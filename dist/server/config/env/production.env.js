ef349d560cef4243f9d388d5484a613980e88d6c
= <izaiasavila@gmail.com>
2017-09-07 02:29:23 -0300

    ..

--- dist/server/config/env/production.env.js
+++ dist/server/config/env/production.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: 'production',
-    dbURL: 'postgres://bpxmmsjyzsolff:e1836a15b911932ecd22de345a65034c1275419c0fc8b59310fba01cde621df3@ec2-107-22-171-11.compute-1.amazonaws.com:5432/d6qb6clibs8ujb',
+    dbURL: 'postgres://123:123@amazonaws.com:5432/d6qb6clibs8ujb',
     secret: 'SeCrEtO100'
 };
